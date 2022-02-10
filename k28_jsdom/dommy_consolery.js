6efb3ba7b8d73f6892409ff3b1ff1a44f93f312d
Zhao Yu Lin <zlin20@stuy.edu>
2022-02-10 00:06:20 -0500

    added k28

--- k28_jsdom/dommy_consolery.js
+++ k28_jsdom/dommy_consolery.js
@@ -24,7 +24,6 @@
 // 2022-02-08t
 // --------------------------------------------------
 
-
 //send diagnostic output to console
 //(Ctrl-Shift-J in Firefox to reveal console)
 console.log("AYO");
@@ -32,73 +31,98 @@ console.log("AYO");
 var i = "hello";
 var j = 20;
 
-
-
 //assign an anonymous fxn to a var
-var f = function(x) {
-  var j=30;
-  return j+x;
+var f = function (x) {
+  var j = 30;
+  return j + x;
 };
 
-
 //instantiate an object
-var o = { 'name' : 'Thluffy',
-          age : 15,
-          items : [10, 20, 30, 40],
-          morestuff : {a : 1, b : 'ayo'},
-          func : function(x) {
-            return x+30;
-          }
-        };
+var o = {
+  name: "Thluffy",
+  age: 15,
+  items: [10, 20, 30, 40],
+  morestuff: { a: 1, b: "ayo" },
+  func: function (x) {
+    return x + 30;
+  },
+};
 
-
-var addItem = function(text) {
+var addItem = function (text) {
   var list = document.getElementById("thelist");
   var newitem = document.createElement("li");
   newitem.innerHTML = text;
   list.appendChild(newitem);
 };
 
-
-var removeItem = function(n) {
-  var listitems = document.getElementsByTagName('li');
+var removeItem = function (n) {
+  var listitems = document.getElementsByTagName("li");
   listitems[n].remove();
 };
 
-
-var red = function() {
+var red = function () {
   var items = document.getElementsByTagName("li");
-  for(var i = 0; i < items.length; i++) {
-    items[i].classList.add('red');
+  for (var i = 0; i < items.length; i++) {
+    items[i].classList.add("red");
   }
 };
 
-
-var stripe = function() {
+var stripe = function () {
   var items = document.getElementsByTagName("li");
-  for(var i = 0; i < items.length; i++) {
-    if (i%2==0){
-      items[i].classList.add('red');
+  for (var i = 0; i < items.length; i++) {
+    if (i % 2 == 0) {
+      items[i].classList.add("red");
     } else {
-      items[i].classList.add('blue');
+      items[i].classList.add("blue");
     }
   }
 };
 
-
 //insert your implementations here for...
 //fact
-var fact = function(n){
-  if(n <= 1)
-  return 1
-  return (n * fact(n - 1))
+var fact = (n) => {
+  if (n <= 1) return 1;
+  return n * fact(n - 1);
 };
+
+function displayFact(n) {
+  let item = document.getElementById("fact");
+  var x = fact(n);
+  console.log(x + "HI");
+  item.innerHTML = `fact(${n}) = ${x}`;
+}
+displayFact(5);
+
+document.getElementById("facButton").addEventListener("click", count);
+
+let count = 0;
+function incrementFact() {
+  count++;
+  fact(count);
+}
 //fib
 var fib = (n) => {
-  if(n <= 1)
-  return n
-  else{
-    return (fib(n - 1) + fib(n - 2))
+  if (n <= 1) return n;
+  else {
+    return fib(n - 1) + fib(n - 2);
   }
 };
+function displayFib(n) {
+  let item = document.getElementById("fib");
+  item.innerHTML = `fib(${n}) = ${fib(n)}`;
+}
+displayFib(12);
 // GCD
+
+var gcd = function (a, b) {
+  let c = Math.max(a, b);
+  let d = Math.min(a, b);
+
+  if (c == 0) return d;
+  if (d == 0) return c;
+  return gcd(d, c % d);
+}; //eucledian GCD
+function displaygcd(a, b) {
+  let item = document.getElementById("fib");
+  item.innerHTML = `gcd(${a}, ${b}) = ${displaygcd(a, b)}`;
+}
